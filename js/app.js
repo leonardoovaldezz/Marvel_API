40a154203bea61ad6a077de5c57130880cb4ed56
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-06-26 20:42:23 -0600

    Localizacion de personajes en mapa

--- js/app.js
+++ js/app.js
@@ -29,12 +29,12 @@ const marvel = {
       for (const { urls, name, thumbnail } of characters) {
         const urlHero = urls[0].url;
         contentHTML += `
-        <div class="col-md-4">
-            <a href="${urlHero}" target="_blank">
-                <img src="${thumbnail.path}.${thumbnail.extension}" alt="${name}" class="img-thumbnail">
-            </a>
-            <h3 class="title">${name}</h3>
-        </div>`;
+<div class="col-md-4">
+    <a href="${urlHero}" target="_blank">
+        <img src="${thumbnail.path}.${thumbnail.extension}" alt="${name}" class="img-thumbnail">
+    </a>
+    <h3 class="title">${name}</h3>
+</div>`;
       }
       container.innerHTML = contentHTML;
     };
@@ -163,63 +163,217 @@ const marvel = {
 
 marvel.render();
 
+const superheroes = {
+  "3DMan": {
+    name: "3-D Man",
+    location: {
+      latitude: 19.4088,
+      longitude: -99.0160
+    }
+  },
+  aBomb: {
+    name: "A-Bomb (HAS)",
+    location: {
+      latitude: 19.4144,
+      longitude: -99.0284
+    }
+  },
+  aim: {
+    name: "A.I.M.",
+    location: {
+      latitude: 19.4206,
+      longitude: -99.0694
+    }
+  },
+  aaronStack: {
+    name: "Aaron Stack",
+    location: {
+      latitude: 19.4043,
+      longitude: -99.0515
+    }
+  },
+  emilBlonsky: {
+    name: "Abomination (Emil Blonsky)",
+    location: {
+      latitude: 19.4069,
+      longitude: -99.0134
+    }
+  },
+  ultimateAbomination: {
+    name: "Abomination (Ultimate)",
+    location: {
+      latitude: 19.4224,
+      longitude: -99.0156
+    }
+  },
+  absorbingMan: {
+    name: "Absorbing Man",
+    location: {
+      latitude: 19.4193,
+      longitude: -98.9787
+    }
+  },
+  abyss: {
+    name: "Abyss",
+    location: {
+      latitude: 19.4065,
+      longitude: -98.9607
+    }
+  },
+  abyssAgeApocalypse: {
+    name: "Abyss (Age of Apocalypse)",
+    location: {
+      latitude: 19.4323,
+      longitude: -99.0102
+    }
+  },
+  adamDestine: {
+    name: "Adam Destine",
+    location: {
+      latitude: 19.4342,
+      longitude: -99.0107
+    }
+  },
+  adamWarlock: {
+    name: "Adam Warlock",
+    location: {
+      latitude: 19.4463,
+      longitude: -99.0328
+    }
+  },
+  aegis: {
+    name: "Aegis (Trey Rollins)",
+    location: {
+      latitude: 19.4059,
+      longitude: -98.9925
+    }
+  },
+  aero: {
+    name: "Aero (Aero)",
+    location: {
+      latitude: 19.4172,
+      longitude: -98.9549
+    }
+  },
+  agathaHarkness: {
+    name: "Agatha Harkness",
+    location: {
+      latitude: 19.4125,
+      longitude: -99.0326
+    }
+  },
+  agentBrand: {
+    name: "Agent Brand",
+    location: {
+      latitude: 19.4160,
+      longitude: -99.0460
+    }
+  },
+  agentX: {
+    name: "Agent X (Nijo)",
+    location: {
+      latitude: 19.4034,
+      longitude: -99.0133
+    }
+  },
+  agentZero: {
+    name: "Agent Zero",
+    location: {
+      latitude: 19.4080,
+      longitude: -99.0027
+    }
+  },
+  agentsOfAtlas: {
+    name: "Agents of Atlas",
+    location: {
+      latitude: 19.4088,
+      longitude: -99.0160
+    }
+  },
+  aginar: {
+    name: "Aginar",
+    location: {
+      latitude: 19.4052,
+      longitude: -99.0346
+    }
+  },
+  airWalker: {
+    name: "Air-Walker (Gabriel Lan)",
+    location: {
+      latitude: 19.4172,
+      longitude: -98.9549
+    }
+  }
+  // Agrega más superhéroes y sus ubicaciones aquí...
+};
 
-
-//MAPA
 const geocodeApiKey = "e795af272354441ca0c66afc46eff58f";
 
-if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(
-    async (position) => {
-      const latitude = position.coords.latitude;
-      const longitude = position.coords.longitude;
+const createMap = (latitude, longitude) => {
+  const mapContainer = document.getElementById("map");
+  const map = L.map(mapContainer).setView([latitude, longitude], 10);
 
-      const response = await fetch(
-        `https://api.opencagedata.com/geocode/v1/json?key=${geocodeApiKey}&q=${latitude},${longitude}`
-      );
-      const data = await response.json();
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors'
+  }).addTo(map);
 
-      if (data.results.length > 0) {
-        const { formatted } = data.results[0];
-        console.log("Dirección:", formatted);
+  return map;
+};
 
-        const mapElement = document.getElementById("map");
+const createMarker = (latitude, longitude, title) => {
+  return L.marker([latitude, longitude]).bindPopup(`<b>${title}</b>`);
+};
 
-        const map = L.map(mapElement).setView([latitude, longitude], 15);
+const fetchUserLocation = async () => {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
 
-        // Agregar capa de mapa base de OpenStreetMap
-        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-          attribution: "Map data © OpenStreetMap contributors",
-        }).addTo(map);
-
-        // Agregar un marcador en la ubicación con la dirección como etiqueta
-        const userLocationMarker = L.marker([latitude, longitude])
-          .addTo(map)
-          .bindPopup(formatted)
-          .openPopup();
-
-        const returnToUserLocation = () => {
-          console.log("Haciendo clic en el botón de regresar");
-          map.setView([latitude, longitude], 15);
-          userLocationMarker.openPopup();
-        };
-
-        const returnButton = document.getElementById("return-button");
-        returnButton.addEventListener("click", returnToUserLocation);
-
-
-        mapElement.appendChild(returnButton);
-
-      } else {
-        console.log(
-          "No se encontró ninguna dirección para las coordenadas proporcionadas."
-        );
-      }
-    },
-    (error) => {
-      console.error("Error al obtener la geolocalización:", error);
-    }
+const getAddressFromCoordinates = async (latitude, longitude) => {
+  const response = await fetch(
+    `https://api.opencagedata.com/geocode/v1/json?key=${geocodeApiKey}&q=${latitude},${longitude}`
   );
-} else {
-  console.error("Geolocalización no compatible");
-}
+  const data = await response.json();
+  if (data.results.length > 0) {
+    const { formatted } = data.results[0];
+    return formatted;
+  } else {
+    throw new Error("No se encontró ninguna dirección para las coordenadas proporcionadas.");
+  }
+};
+
+const renderMap = async () => {
+  try {
+    const userPosition = await fetchUserLocation();
+    const { latitude, longitude } = userPosition.coords;
+
+    const userAddress = await getAddressFromCoordinates(latitude, longitude);
+    console.log("Dirección:", userAddress);
+
+    const map = createMap(latitude, longitude);
+
+    // Agregar un marcador en la ubicación del usuario con la dirección como etiqueta
+    const userLocationMarker = createMarker(latitude, longitude, userAddress).addTo(map).openPopup();
+
+    const returnToUserLocation = () => {
+      console.log("Haciendo clic en el botón de regresar");
+      map.setView([latitude, longitude], 15);
+      userLocationMarker.openPopup();
+    };
+
+    const returnButton = document.getElementById("return-button");
+    returnButton.addEventListener("click", returnToUserLocation);
+
+    // Iterar sobre los superhéroes y crear marcadores en el mapa para cada ubicación
+    for (const superheroKey in superheroes) {
+      const superhero = superheroes[superheroKey];
+      const { latitude, longitude } = superhero.location;
+      const superheroMarker = createMarker(latitude, longitude, superhero.name).addTo(map);
+    }
+  } catch (error) {
+    console.error("Error al obtener la geolocalización:", error);
+  }
+};
+
+renderMap();

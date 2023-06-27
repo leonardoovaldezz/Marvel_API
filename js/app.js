8e80a866278cf973badb565bcf53558574ba32b3
leonardoovaldezz <leonardocamvaldez12@hotmail.com>
2023-06-26 18:08:12 -0600

    Localizacion superheroe cercano

--- js/app.js
+++ js/app.js
@@ -15,28 +15,157 @@ const marvel = {
       `characters?limit=20&offset=${offset}&ts=${getTimestamp()}&apikey=${publicKey}&hash=${createHash()}`;
 
     const container = document.querySelector("#marvel-row");
-    let contentHTML = "";
+    const searchInput = document.querySelector("#search-input");
+    const searchButton = document.querySelector("#search-button");
 
-    const response = await fetch(`${URL_API_MARVEL}${createParams()}`);
-    const { data } = await response.json();
+    const fetchCharacters = async () => {
+      const response = await fetch(`${URL_API_MARVEL}${createParams()}`);
+      const { data } = await response.json();
+      return data.results;
+    };
 
-    for (const { urls, name, thumbnail } of data.results) {
-      const urlHero = urls[0].url;
-      contentHTML += `
+    const renderCharacters = (characters) => {
+      let contentHTML = "";
+      for (const { urls, name, thumbnail } of characters) {
+        const urlHero = urls[0].url;
+        contentHTML += `
         <div class="col-md-4">
             <a href="${urlHero}" target="_blank">
                 <img src="${thumbnail.path}.${thumbnail.extension}" alt="${name}" class="img-thumbnail">
             </a>
             <h3 class="title">${name}</h3>
         </div>`;
-    }
+      }
+      container.innerHTML = contentHTML;
+    };
 
-    container.innerHTML = contentHTML;
+    const superheroes = {
+      "3DMan": {
+        name: "3-D Man",
+        location: {
+          latitude: 19.2970,
+          longitude: -99.6352
+
+        }
+      },
+      aBomb: {
+        name: "A-Bomb (HAS)",
+        location: {
+          latitude: 19.4326,
+          longitude: -99.1332
+        }
+      },
+      aim: {
+        name: "A.I.M.",
+        location: {
+          latitude: 19.4326,
+          longitude: -99.1332
+        }
+      },
+      // Agrega más superhéroes y sus ubicaciones aquí...
+    };
+
+    const findClosestSuperhero = (userLatitude, userLongitude) => {
+      let closestSuperhero = null;
+      let closestDistance = Infinity;
+
+      for (const superheroKey in superheroes) {
+        const superhero = superheroes[superheroKey];
+        const superheroLatitude = superhero.location.latitude;
+        const superheroLongitude = superhero.location.longitude;
+
+        const distance = calculateDistance(
+          userLatitude,
+          userLongitude,
+          superheroLatitude,
+          superheroLongitude
+        );
+
+        if (distance < closestDistance) {
+          closestSuperhero = superhero;
+          closestDistance = distance;
+        }
+      }
+
+      return closestSuperhero;
+    };
+
+    const calculateDistance = (lat1, lon1, lat2, lon2) => {
+      const earthRadius = 6371;
+      const dLat = toRadians(lat2 - lat1);
+      const dLon = toRadians(lon2 - lon1);
+
+      const a =
+        Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(lat1)) *
+        Math.cos(toRadians(lat2)) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+      const distance = earthRadius * c;
+
+      return distance;
+    };
+
+    const toRadians = (degrees) => {
+      return degrees * (Math.PI / 180);
+    };
+
+    const searchCharacters = async () => {
+      const characters = await fetchCharacters();
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredCharacters = characters.filter((character) =>
+        character.name.toLowerCase().includes(searchTerm)
+      );
+      if (filteredCharacters.length === 0) {
+        alert("No se encontraron coincidencias.");
+        location.reload(); // Recargar la página
+      }
+      renderCharacters(filteredCharacters);
+    };
+
+    searchButton.addEventListener("click", searchCharacters);
+
+    const characters = await fetchCharacters();
+    renderCharacters(characters);
+
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const userLatitude = position.coords.latitude;
+          const userLongitude = position.coords.longitude;
+
+          const closestSuperhero = findClosestSuperhero(
+            userLatitude,
+            userLongitude
+          );
+
+          if (closestSuperhero) {
+            console.log(
+              "El superhéroe más cercano es:",
+              closestSuperhero.name
+            );
+            // Aquí puedes utilizar la API de Marvel para obtener más información sobre el superhéroe más cercano
+            // y mostrarla en tu aplicación
+          } else {
+            console.log("No se encontró ningún superhéroe cercano.");
+          }
+        },
+        (error) => {
+          console.error("Error al obtener la geolocalización:", error);
+        }
+      );
+    } else {
+      console.error("Geolocalización no compatible");
+    }
   },
 };
 
 marvel.render();
 
+
+
+//MAPA
 const geocodeApiKey = "e795af272354441ca0c66afc46eff58f";
 
 if ("geolocation" in navigator) {
